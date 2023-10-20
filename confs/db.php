fe459ccf523dc5478859374f799f56cc6d67f91e
Samuel Domin <samuel.domin@dominanz.sk>
2023-10-20 20:17:27 +0200

    first commit

---
+++ confs/db.php
@@ -0,0 +1,9 @@
+<?php
+    $user = "rezervacny_syst1";
+    $pass = "f2Hugfyb";
+    $port = 3306;
+    $server = "db.dw082.nameserver.sk";
+    $db = "rezervacny_system";
+
+    $conn = mysqli_connect($server,$user,$pass,$db,$port);
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<html>
+    <head></head>
+    <body>
+        <h1>Vitajte v rezervacnom systeme MTF.</h1>
+    </body>
+</html>
