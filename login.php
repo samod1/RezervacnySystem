e49ab54309d84493194a5337289dd5c2fad21abd
Samuel Domin <samuel.domin@dominanz.sk>
2023-10-21 08:36:44 +0200

    pridanie prihlasovacieho a registracneho formularu

---
+++ login.php
@@ -0,0 +1,10 @@
+<body>
+    <form method="post">
+        <label for="username">Pouužívateľské meno</label>
+        <<input type="text" name="username" id="username">
+
+        <label for="username">Pouužívateľské meno</label>
+        <<input type="password" name="password" id="password">
+
+    </form>
+</body>
