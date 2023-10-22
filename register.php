bf2a32247d753b8e2ac89b146689bc8ff881d5ff
Samuel Domin <samuel.domin@dominanz.sk>
2023-10-22 20:20:08 +0200

    registracny formular registracna routina

--- register.php
+++ register.php
@@ -3,11 +3,49 @@
 ?>
 
 <div class="row">
-    <div class="col-3"></div>
-    <div class="col-3">
+    <div class="col-4"></div>
+    <div class="col-4">
             <form class="form-group" method="POST" action="routines/register/registerRout.php">
-                <input type="text" name="username" required>
+                <div class="form-group">
+                    <label>Prihlasovacie meno</label>
+                    <input class="form-control form-control-lg" type="text" name="username" required>
+                </div>
+                <div class="form-group">
+                <label>Heslo</label>
+                <input class="form-control form-control-lg" type="password" name="password" required>
+                </div>
+                <div class="form-group">
+                    <label>Adresa</label>
+                    <input class="form-control form-control-lg" type="text" name="address" required>
+                </div>
+                <div class="form-group">
+                    <label>ICO</label>
+                    <input class="form-control form-control-lg" type="text" name="ICO">
+                </div>
+                <div class="form-group">
+                    <label>DIC</label>
+                    <input class="form-control form-control-lg" type="text" name="DIC">
+                </div>
+                <div class="form-group">
+                    <label>E-mail</label>
+                    <input class="form-control form-control-lg" type="email" name="email" required>
+                </div>
+                <div class="form-group">
+                    <label>Telefonne cislo</label>
+                    <input class="form-control form-control-lg" type="text" name="telefonne_cislo" required>
+                </div>
+                <div class="form-group">
+                    <div class="row">
+                        <div class="col-6">
+                            <input type="submit" class="btn btn-lg btn-block btn-primary" name="submit"> 
+                        </div>
+                        <div class="col-6">
+                            <input class="btn btn-lg btn-block btn-secondary" type="reset">
+                        </div>
+                        
+                    </div>
+                </div>
             </form>
     </div>
-    <div class="col-3"></div>
+    <div class="col-4"></div>
 </div>
--- routines/register/registerRout.php
+++ routines/register/registerRout.php
@@ -1,27 +1,57 @@
 <?php 
     /**DB include*/
     $conn ='';
-    include "confs/db.php";
+    include "db.php";
+
+    if(isset($_POST["submit"]))
+    {
+
+        if(isset($_POST["ICO"]))
+        {
+            $user_role='firm';
+            $query = 'INSERT INTO user (id_user, username, password, address, ICO, DIC, email, telefonne_cislo, user_role) VALUES(?,?,?,?,?,?,?,?,?)';
+            $id=0;
+            $username = $_POST["username"];
+            $password = $_POST["password"];
+            $address = $_POST["address"];
+            $ico = $_POST["ICO"];
+            $dic = $_POST["DIC"];
+            $email = $_POST["email"];
+            $tel = $_POST["telefonne_cislo"];
+            $user_role = $_POST["user_role"];
 
 
-    $query = 'INSERT INTO user (id_user, username, password, address, ICO, DIC, email, telefonne_cislo, user_role) VALUES(?,?,?,?,?,?,?,?,?)';
-    $id=0;
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    $address = $_POST["address"];
-    $ico = $_POST["ICO"];
-    $dic = $_POST["DIC"];
-    $email = $_POST["email"];
-    $tel = $_POST["telefonne_cislo"];
-    $user_role = $_POST["user_role"];
+            $stmt = mysqli_stmt_init($conn);
+            mysqli_stmt_prepare($stmt,$query);
+            mysqli_stmt_bind_param($stmt,'isssissss',$id,$username,$password,$address,$ico,$dic,$email,$tel,$user_role);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_close($stmt);
+        }
+    else
+    {
+        $user_role='student';
+        $query = 'INSERT INTO user (id_user, username, password, address, ICO, DIC, email, telefonne_cislo, user_role) VALUES(?,?,?,?,?,?,?,?,?)';
+        $id=0;
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        $address = $_POST["address"];
+        $ico = $_POST["ICO"];
+        $dic = $_POST["DIC"];
+        $email = $_POST["email"];
+        $tel = $_POST["telefonne_cislo"];
+        $user_role = $_POST["user_role"];
 
 
-    $stmt = mysqli_stmt_init($conn);
-    mysqli_stmt_prepare($stmt,$query);
-    mysqli_stmt_bind_param($stmt,'isssissss',$id,$username,$password,$address,$ico,$dic,$email,$tel,$user_role);
-    mysqli_stmt_execute($stmt);
-    mysqli_stmt_close($stmt);
+        $stmt = mysqli_stmt_init($conn);
+        mysqli_stmt_prepare($stmt,$query);
+        mysqli_stmt_bind_param($stmt,'isssissss',$id,$username,$password,$address,$ico,$dic,$email,$tel,$user_role);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_close($stmt);
+    }    
+        
+    }
     
+
 
 
 ?>
