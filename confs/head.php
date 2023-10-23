727d172a2d5c7161c7cf1d8b06ad03c4f47c3853
Samuel Domin <samuel.domin@dominanz.sk>
2023-10-23 13:31:43 +0200

    Pridanie automatickeho pripajania na databazu

--- confs/head.php
+++ confs/head.php
@@ -3,7 +3,23 @@
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 </body>
+<?php
+ $conn ='';
+ include "db.php";
+?>
 <div class="container">
     <div class="jumbotron">
         <h1 style="text-align:center;" class="display-4">Rezervacny system MTF</h1>
+        
     </div>
+    <?php
+        if(isset($_SESSION['username']))
+            {
+                include "navbar_for_signed.php";
+            }
+        else
+        {
+            
+        }
+            
+        ?>
