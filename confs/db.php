dee38947db34bf4514e4a58041bf8585947fdb89
Samuel Domin <samuel.domin@dominanz.sk>
2023-10-23 13:31:27 +0200

    Pridanie overovacej hlasky k DB

--- confs/db.php
+++ confs/db.php
@@ -6,4 +6,24 @@
     $db = "rezervacny_system";
 
     $conn = mysqli_connect($server,$user,$pass,$db,$port);
+
+    if(!$conn)
+    {
+        echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+        Pripojenie na databazu <b>". $server . "</b> zlyhalo
+        <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+          <span aria-hidden='true'>&times;</span>
+        </button>
+      </div>";
+    }
+
+    else
+    {
+        echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+        Pripojenie na databazu <b>". $server . "</b> prebehlo uspesne
+        <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+          <span aria-hidden='true'>&times;</span>
+        </button>
+      </div>";
+    }
 ?>
