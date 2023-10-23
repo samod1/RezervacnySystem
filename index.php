87879d12f89ea027c454e59505363d16a0cd2a90
Samuel Domin <samuel.domin@dominanz.sk>
2023-10-23 13:30:28 +0200

    overovanie ci je nastaveny session

--- index.php
+++ index.php
@@ -4,5 +4,9 @@
         <h1>Vitajte v rezervacnom systeme MTF.</h1>
         <a href="register.php" type="button">Registracia</a>
         <a href="login.php" type="button">Prihlasenie</a>
+
+        <?php
+        echo $_SESSION['username'];
+        ?>
     </body>
 </html>
