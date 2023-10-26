2eb2559cc51885e5eeabb5055f5ea010622f0e9a
Samuel Domin <samuel.domin@dominanz.sk>
2023-10-26 07:33:12 +0200

    delete routine

--- routines/common/delete.php
+++ routines/common/delete.php
@@ -13,8 +13,39 @@
         mysqli_stmt_execute($stmt);
         mysqli_stmt_close($stmt);
 
+        
         header("Location: ../../buildings.php");
     }
+
+    if(isset($_GET['id']) && $_GET['type'] == 'room')
+    {
+        $id = $_GET['id'];
+        $query = "DELETE FROM rezervacny_system.building WHERE id_building=?";
+
+        $stmt = mysqli_stmt_init($conn);
+        mysqli_stmt_prepare($stmt, $query);
+        mysqli_stmt_bind_param($stmt,'i', $id);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_close($stmt);
+
+        header("Location: ../../buildings.php");
+    }
+
+    if(isset($_GET['id']) && $_GET['type'] == 'table')
+    {
+        $id = $_GET['id'];
+        $query = "DELETE FROM rezervacny_system.building WHERE id_building=?";
+
+        $stmt = mysqli_stmt_init($conn);
+        mysqli_stmt_prepare($stmt, $query);
+        mysqli_stmt_bind_param($stmt,'i', $id);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_close($stmt);
+
+        header("Location: ../../buildings.php");
+    }
+
+    
     
 
 ?>
